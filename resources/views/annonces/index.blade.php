da146c126c75ed3c31319409a7e55dded2667d1e
Mimachh <karl.mullr@gmail.com>
2022-11-10 16:06:01 +0100

    ajout des favoris dans la page dashboard, reste à mettre en grid les annonces classiques et regler le problème des photos, des villes et des animaux en clés étrangères

--- resources/views/annonces/index.blade.php
+++ resources/views/annonces/index.blade.php
@@ -7,9 +7,9 @@
     </x-slot>
 
         @section('content')
-        <h1 class="text-3xl text-green-500 mb-3">coucou</h1>
+       
         
-        <div class="md:flex-wrap md:flex md:mx-32 xs:mx-auto">
+        <div class="md:flex-wrap md:flex md:mx-32 xs:mx-auto mt-5">
       
         @foreach($annonces as $annonce)
    
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,16 +2,48 @@
 
     <x-slot name="header">
     
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('Tableau de bord/Accueil plutôt') }}
+            <h2 class="font-semibold text-xl text-white leading-tight">
+                {{ __('Tableau de bord de ') }}{{auth()->user()->name}}
             </h2>
      
     </x-slot>
 
-    <div class="py-12 bg-red-700 ">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 opacity-50">
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 opacity-80">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+              <h1 class="ml-5 mt-5 font-semibold text-lg text-gray-800">Mes favoris</h1>
+              <hr class="my-5 mx-5 border border-2 border-gray-400">
+                @foreach(auth()->user()->fav as $fav)
+                <div class="px-3 py-5 mb-3 mr-32 ml-5 shadow-sm hover:shadow-md rounded border border-gray-200">
+                    <div class="flex justify-between">
+                        <h2 class="text-md font-bold text-gray-600">{{$fav->name}}</h2>
+                        <button class="h-5 w-5 text-red-400 focus:outline-none" wire:click="add_fav">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="{{ $fav->fav_added() ? 'red' : 'white'}}" viewBox="0 0 24 24"  stroke="red" >
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                            </svg>
+                        </button>
+                    </div>
+                   <p class="text-sm text-gray-600 pb-2">Disponible du : <span class="text-md text-gray-800">{{$fav->start_watch}}</span>  au : <span class="text-md text-gray-800">{{$fav->end_watch}} </span></p>
+                   <p class="text-sm text-gray-600 pb-2"> Ville : {{ $fav->ville }}</p>
+                    
+                    <p class="text-sm text-gray-600 pb-2">Type de garde :
+                        @if ($fav->garde_type == 1)
+                        <span class="text-sm text-gray-800">Au domicile du Pet-Sitter</span> 
+                        @elseif ($fav->garde_type == 2)
+                        <span class="text-sm text-gray-800">En visite à votre domicile</> 
+                        @elseif ($fav->garde_type == 3)
+                        <span class="text-sm text-gray-800"> En visite et chez le Pet-Sitter</span>
+                        @endif
+                    </p>
+                        
+                    <p class="text-sm text-gray-600 pb-2">Animaux gardés :  </p>    
+                    <p class="text-sm text-gray-600 pb-2">Prix : <span class="text-sm text-gray-800">{{ number_format($fav->price, 2, ',', '') }} €/jour.</span></p>     
+                    <div class="flex items-center">
+                        <span class="h-2 w-2 bg-green-600 rounded-full mr-1"></span>
+                        <a class="text-sm" href="">Voir l'annonce</a>
+                    </div> 
+                </div>
+                @endforeach
             </div>
         </div>
     </div>
